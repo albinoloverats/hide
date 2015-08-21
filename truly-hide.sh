a8908b89f93c7e4626908b5cc3485e917d4de893
The hide Development Team <hide@albinoloverats.net>
2015-08-21 09:42:01 +0100

    Add helper script
    This script will encrypt a file before hiding it, and decrypt it again
    when recovering it. It uses the --raw option for encrypt, which causes
    encrypt not to generate a header, and the -f fill option for hide, which
    does not prefix the file size to the hidden data as well as pad with
    random noise the entire image.

---
+++ truly-hide.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+function usage
+{
+	echo "$0 <image> <file>"
+	echo "$0 <image>"
+	exit -1
+}
+
+function needed
+{
+	echo "You need encrypt installed, and valid defaults set in ~/.encryptrc"
+	exit -1
+}
+
+test $# -ne 1 -a $# -ne 2 && usage
+test -f $HOME/.encryptrc || needed
+
+tmp=$(mktemp)
+image=$1
+
+if test $# -eq 2
+then
+	file=$2
+	encrypt --nogui --raw $2 $tmp
+
+	out=$(basename $image)
+	ext=${out##*.}
+	out=${out%.*}
+
+	./hide -f $image $tmp $out-2.$ext
+else
+	./hide -f $image $tmp
+	decrypt --nogui --raw $tmp .
+fi
+
+unlink $tmp
